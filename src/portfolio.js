8cacd21b7ea150b7c5d00427b1a7583c063ece8c
Kailas Walldoddi <kailaswalldoddi@Kailass-MacBook-Pro.local>
2022-10-17 21:30:40 +0530

    added logo initials

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://kailashw.github.io',
-  title: 'JS.',
+  title: '-KW',
 }
 
 const about = {
