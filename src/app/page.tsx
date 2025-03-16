fc25c440578f7979efe24b201e8955426e47cfe6
Kailas Walldoddi <kailas.walldoddi@locai.ai>
2025-03-17 03:33:59 +0400

    adding meta data for SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ import ProfileCard from "@/components/profileCard";
 import Quotes from "@/components/quote";
 import TechStack from "@/components/technologies";
 import { useTheme } from "@/context/ThemeContext";
+import Head from "next/head";
 import React from "react";
 
-
 export default function Home() {
-  const { isDarkMode} = useTheme();
+  const { isDarkMode } = useTheme();
 
   return (
     <div
@@ -17,6 +17,14 @@ export default function Home() {
         isDarkMode ? "bg-gray-900 text-white" : "bg-white text-gray-900"
       }`}
     >
+      <Head>
+        <title>Kailas Walldoddi | Developer</title>
+        <meta
+          name="Kailas Walldoddi"
+          content="Python | LLM | React JS | Node JS| Nest JS | Next JS | Cloud | AWS | Azure | GCP | Docker | Fast API | Flask | Express JS | Mongo DB | Postgres"
+          key="Python', 'LLM', 'React JS', 'Node JS| Nest JS', 'Next JS', 'Cloud', 'AWS', 'Azure', 'GCP', 'Docker', 'Fast API', 'Flask', 'Express JS', 'Mongo DB', 'Postgres"
+        />
+      </Head>
       <Header />
       <main className="container mx-auto px-4 py-20">
         <ProfileCard />
